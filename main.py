70fd158d3f2c5217d8ede09b9fb736e62b64e18d
MrBubune <51889542+MrBubune@users.noreply.github.com>
2025-07-15 15:32:55 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+import math
+import pygame
+
+pygame.init()
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption('Planet Simulation')
+clock = pygame.time.Clock()
